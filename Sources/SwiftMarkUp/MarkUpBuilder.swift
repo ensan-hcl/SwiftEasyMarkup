308a80ef788e199241e4f263805509e7ae185566
ensan <ensan.hcl.dev@gmail.com>
2021-09-12 23:31:24 +0900

    make things more public

--- Sources/SwiftMarkUp/MarkUpBuilder.swift
+++ Sources/SwiftMarkUp/MarkUpBuilder.swift
@@ -27,23 +27,26 @@ extension Group: MarkUpView where Content: View {}
 
 @resultBuilder
 public struct MarkUpBuilder {
-    static func buildBlock<T0: MarkUpView> (_ t0: T0) -> some MarkUpView {
+    public static func buildBlock<T0: MarkUpView> (_ t0: T0) -> some MarkUpView {
         return t0
     }
-    static func buildBlock<T0: MarkUpView, T1: MarkUpView> (_ t0: T0, _ t1: T1) -> some MarkUpView {
+
+    public static func buildBlock<T0: MarkUpView, T1: MarkUpView> (_ t0: T0, _ t1: T1) -> some MarkUpView {
         return Group {
             t0
             t1
         }
     }
-    static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2) -> some MarkUpView {
+
+    public static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2) -> some MarkUpView {
         return Group {
             t0
             t1
             t2
         }
     }
-    static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3) -> some MarkUpView {
+
+    public static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3) -> some MarkUpView {
         return Group {
             t0
             t1
@@ -51,7 +54,8 @@ public struct MarkUpBuilder {
             t3
         }
     }
-    static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView, T4: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3, _ t4: T4) -> some MarkUpView {
+
+    public static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView, T4: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3, _ t4: T4) -> some MarkUpView {
         return Group {
             t0
             t1
@@ -60,7 +64,8 @@ public struct MarkUpBuilder {
             t4
         }
     }
-    static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView, T4: MarkUpView, T5: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3, _ t4: T4, _ t5: T5) -> some MarkUpView {
+
+    public static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView, T4: MarkUpView, T5: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3, _ t4: T4, _ t5: T5) -> some MarkUpView {
         return Group {
             t0
             t1
@@ -70,7 +75,8 @@ public struct MarkUpBuilder {
             t5
         }
     }
-    static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView, T4: MarkUpView, T5: MarkUpView, T6: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3, _ t4: T4, _ t5: T5, _ t6: T6) -> some MarkUpView {
+
+    public static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView, T4: MarkUpView, T5: MarkUpView, T6: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3, _ t4: T4, _ t5: T5, _ t6: T6) -> some MarkUpView {
         return Group {
             t0
             t1
@@ -82,7 +88,7 @@ public struct MarkUpBuilder {
         }
     }
 
-    static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView, T4: MarkUpView, T5: MarkUpView, T6: MarkUpView, T7: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3, _ t4: T4, _ t5: T5, _ t6: T6, _ t7: T7) -> some MarkUpView {
+    public static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView, T4: MarkUpView, T5: MarkUpView, T6: MarkUpView, T7: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3, _ t4: T4, _ t5: T5, _ t6: T6, _ t7: T7) -> some MarkUpView {
         return Group {
             t0
             t1
@@ -95,7 +101,7 @@ public struct MarkUpBuilder {
         }
     }
 
-    static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView, T4: MarkUpView, T5: MarkUpView, T6: MarkUpView, T7: MarkUpView, T8: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3, _ t4: T4, _ t5: T5, _ t6: T6, _ t7: T7, _ t8: T8) -> some MarkUpView {
+    public static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView, T4: MarkUpView, T5: MarkUpView, T6: MarkUpView, T7: MarkUpView, T8: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3, _ t4: T4, _ t5: T5, _ t6: T6, _ t7: T7, _ t8: T8) -> some MarkUpView {
         return Group {
             t0
             t1
@@ -109,7 +115,7 @@ public struct MarkUpBuilder {
         }
     }
 
-    static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView, T4: MarkUpView, T5: MarkUpView, T6: MarkUpView, T7: MarkUpView, T8: MarkUpView, T9: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3, _ t4: T4, _ t5: T5, _ t6: T6, _ t7: T7, _ t8: T8, _ t9: T9) -> some MarkUpView {
+    public static func buildBlock<T0: MarkUpView, T1: MarkUpView, T2: MarkUpView, T3: MarkUpView, T4: MarkUpView, T5: MarkUpView, T6: MarkUpView, T7: MarkUpView, T8: MarkUpView, T9: MarkUpView> (_ t0: T0, _ t1: T1, _ t2: T2, _ t3: T3, _ t4: T4, _ t5: T5, _ t6: T6, _ t7: T7, _ t8: T8, _ t9: T9) -> some MarkUpView {
         return Group {
             t0
             t1
@@ -124,23 +130,23 @@ public struct MarkUpBuilder {
         }
     }
 
-    static func buildEither<T0: MarkUpView>(first t0: T0) -> T0 {
+    public static func buildEither<T0: MarkUpView>(first t0: T0) -> T0 {
         return t0
     }
 
-    static func buildEither<T0: MarkUpView>(second t0: T0) -> T0 {
+    public static func buildEither<T0: MarkUpView>(second t0: T0) -> T0 {
         return t0
     }
 
-    static func buildLimitedAvailability<T0: MarkUpView>(_ t0: T0) -> T0 {
+    public static func buildLimitedAvailability<T0: MarkUpView>(_ t0: T0) -> T0 {
         return t0
     }
 
-    static func buildExpression(_ expression: LocalizedStringKey) -> MarkUpText {
+    public static func buildExpression(_ expression: LocalizedStringKey) -> MarkUpText {
         return MarkUpText(expression)
     }
 
-    static func buildExpression<T0: MarkUpView>(_ expression: T0) -> T0 {
+    public static func buildExpression<T0: MarkUpView>(_ expression: T0) -> T0 {
         return expression
     }
 
